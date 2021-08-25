1e22218643cf6eeccd9b10b43ae498352c61f917
PatrickStar720 <77839637+PatrickStar720@users.noreply.github.com>
2021-08-26 00:03:04 +0700

    Add files via upload

--- database/language/ind.js
+++ database/language/ind.js
@@ -87,295 +87,283 @@ exports.clears = () => {
 //====================BATES NGABBBB==================\\
        
 exports.owner = (prefix) => {
-	return `▪️ *Owner Menu*
-${c}│ ❏ ${prefix}event${c} 1 | 0
-${c}│ ❏ ${prefix}receive${c}
-${c}│ ❏ ${prefix}setppbot${c}
-${c}│ ❏ ${prefix}bc${c}
-${c}│ ❏ ${prefix}bcgc${c}
-${c}│ ❏ ${prefix}setreply${c}
-${c}│ ❏ ${prefix}setthumb${c}
-${c}│ ❏ ${prefix}antidelete${c}
-${c}│ ❏ ${prefix}setmemberlimit${c}
-${c}│ ❏ ${prefix}setlimit${c}
-${c}│ ❏ ${prefix}setkey${c}
-${c}│ ❏ ${prefix}addprem${c}
-${c}│ ❏ ${prefix}unprem${c}
-${c}│ ❏ ${prefix}ban${c}
-${c}│ ❏ ${prefix}unban${c}
-${c}│ ❏ ${prefix}setprefix${c}
-${c}│ ❏ ${prefix}clearall${c}
-${c}│ ❏ ${prefix}block${c}
-${c}│ ❏ ${prefix}unblock${c} 
-${c}│ ❏ ${prefix}leave${c}
-${c}│ ❏ ${prefix}clone${c}
-${c}│ ❏ ${prefix}setppbot${c}
-${c}│ ❏ ${prefix}readall${c}
-${c}╰─────────────────${c}         
+	return `◉ *Owner Menu*
+${c} ❏ ${prefix}receive${c}
+${c}  ❏ ${prefix}setppbot${c}
+${c}  ❏ ${prefix}bc${c}
+${c}  ❏ ${prefix}bcgc${c}
+${c}  ❏ ${prefix}setreply${c}
+${c}  ❏ ${prefix}setthumb${c}
+${c}  ❏ ${prefix}antidelete${c}
+${c}  ❏ ${prefix}setmemberlimit${c}
+${c}  ❏ ${prefix}setlimit${c}
+${c}  ❏ ${prefix}addprem${c}
+${c}  ❏ ${prefix}unprem${c}
+${c}  ❏ ${prefix}ban${c}
+${c}  ❏ ${prefix}unban${c}
+${c}  ❏ ${prefix}setprefix${c}
+${c}  ❏ ${prefix}clearall${c}
+${c}  ❏ ${prefix}block${c}
+${c}  ❏ ${prefix}unblock${c} 
+${c}  ❏ ${prefix}leave${c}
+${c}  ❏ ${prefix}clone${c}
+${c}  ❏ ${prefix}setppbot${c}
+${c}  ❏ ${prefix}readall${c}
           
        ~ PatrickBot 2021`
 }
 
 exports.maker = (prefix) => {
-	return `▪️ *Maker Menu*
-${c}│ ❏ ${prefix}csgo${c}
-${c}│ ❏ ${prefix}honey${c}
-${c}│ ❏ ${prefix}silk${c} 
-${c}│ ❏ ${prefix}warface${c}
-${c}│ ❏ ${prefix}gradient${c}
-${c}│ ❏ ${prefix}neon${c}
-${c}│ ❏ ${prefix}matrix${c}
-${c}│ ❏ ${prefix}butterfly${c}
-${c}│ ❏ ${prefix}slide${c} 
-${c}│ ❏ ${prefix}party${c}
-${c}│ ❏ ${prefix}glowmetalic${c}
-${c}│ ❏ ${prefix}rainbowshine${c}
-${c}│ ❏ ${prefix}bokeh${c}
-${c}│ ❏ ${prefix}hartatahta${c} 
-${c}│ ❏ ${prefix}biscuit${c}
-${c}│ ❏ ${prefix}3dunderwater${c} 
-${c}│ ❏ ${prefix}3dglowing${c}
-${c}│ ❏ ${prefix}bearlogo${c}
-${c}│ ❏ ${prefix}cloud${c}
-${c}│ ❏ ${prefix}steel${c}
-${c}│ ❏ ${prefix}8bit${c}
-${c}│ ❏ ${prefix}fruitjuice${c}
-${c}│ ❏ ${prefix}icecold${c}
-${c}│ ❏ ${prefix}marble${c}
-${c}│ ❏ ${prefix}horror${c}
-${c}│ ❏ ${prefix}wood${c}
-${c}│ ❏ ${prefix}greenneon${c}
-${c}│ ❏ ${prefix}breakwall${c}
-${c}│ ❏ ${prefix}pubg${c} 
-${c}│ ❏ ${prefix}flaming${c}
-${c}│ ❏ ${prefix}naruto${c}
-${c}│ ❏ ${prefix}brokenglass${c}
-${c}│ ❏ ${prefix}glass${c}
-${c}│ ❏ ${prefix}attp${c}
-${c}│ ❏ ${prefix}makequote${c}
-${c}│ ❏ ${prefix}ttp${c}
-${c}│ ❏ ${prefix}ocr${c} 
-${c}╰─────────────────${c}
+	return `◉ *Maker Menu*
+${c}  ❏ ${prefix}csgo${c}
+${c}  ❏ ${prefix}honey${c}
+${c}  ❏ ${prefix}silk${c} 
+${c}  ❏ ${prefix}warface${c}
+${c}  ❏ ${prefix}gradient${c}
+${c}  ❏ ${prefix}neon${c}
+${c}  ❏ ${prefix}matrix${c}
+${c}  ❏ ${prefix}butterfly${c}
+${c}  ❏ ${prefix}slide${c} 
+${c}  ❏ ${prefix}party${c}
+${c}  ❏ ${prefix}glowmetalic${c}
+${c}  ❏ ${prefix}rainbowshine${c}
+${c}  ❏ ${prefix}bokeh${c}
+${c}  ❏ ${prefix}hartatahta${c} 
+${c}  ❏ ${prefix}biscuit${c}
+${c}  ❏ ${prefix}3dunderwater${c} 
+${c}  ❏ ${prefix}3dglowing${c}
+${c}  ❏ ${prefix}bearlogo${c}
+${c}  ❏ ${prefix}cloud${c}
+${c}  ❏ ${prefix}steel${c}
+${c}  ❏ ${prefix}8bit${c}
+${c}  ❏ ${prefix}fruitjuice${c}
+${c}  ❏ ${prefix}icecold${c}
+${c}  ❏ ${prefix}marble${c}
+${c}  ❏ ${prefix}horror${c}
+${c}  ❏ ${prefix}wood${c}
+${c}  ❏ ${prefix}greenneon${c}
+${c}  ❏ ${prefix}breakwall${c}
+${c}  ❏ ${prefix}pubg${c} 
+${c}  ❏ ${prefix}flaming${c}
+${c}  ❏ ${prefix}naruto${c}
+${c}  ❏ ${prefix}brokenglass${c}
+${c}  ❏ ${prefix}glass${c}
+${c}  ❏ ${prefix}attp${c}
+${c}  ❏ ${prefix}makequote${c}
+${c}  ❏ ${prefix}ttp${c}
+${c}  ❏ ${prefix}ocr${c} 
           
        ~ PatrickBot 2021`
 }
 
 exports.game = (prefix) => {
-	return `▪️ *Game Menu*
-${c}│ ❏ ${prefix}tebakgambar${c}
-${c}│ ❏ ${prefix}math${c}
-${c}│ ❏ ${prefix}tebaksiapa${c}
-${c}│ ❏ ${prefix}family100${c}
-${c}╰─────────────────${c}
+	return `◉ *Game Menu*
+${c}  ❏ ${prefix}tebakgambar${c}
+${c}  ❏ ${prefix}math${c}
+${c}  ❏ ${prefix}tebaksiapa${c}
+${c}  ❏ ${prefix}family100${c}
           
        ~ PatrickBot 2021`
 }
 
 exports.kerang = (prefix) => {
-	return `▪️ *Kerang Menu*
-${c}│ ❏ ${prefix}apakah${c}
-${c}│ ❏ ${prefix}bisakah${c}
-${c}│ ❏ ${prefix}kapankah${c}
-${c}│ ❏ ${prefix}rate${c}
-${c}│ ❏ ${prefix}ganteng${c}
-${c}│ ❏ ${prefix}beban${c}
-${c}│ ❏ ${prefix}cantik${c}
-${c}│ ❏ ${prefix}jelek${c}
-${c}│ ❏ ${prefix}nggagur${c}
-${c}│ ❏ ${prefix}baik${c}
-${c}│ ❏ ${prefix}jahat${c}
-${c}│ ❏ ${prefix}nolep${c}
-${c}│ ❏ ${prefix}wibu${c}
-${c}│ ❏ ${prefix}hebat${c}
-${c}│ ❏ ${prefix}pakboy${c}
-${c}│ ❏ ${prefix}pakgirl${c}
-${c}│ ❏ ${prefix}vganteng${c}
-${c}│ ❏ ${prefix}vbeban${c}
-${c}│ ❏ ${prefix}vcantik${c}
-${c}│ ❏ ${prefix}vjelek${c}
-${c}│ ❏ ${prefix}vnggagur${c}
-${c}│ ❏ ${prefix}vbaik${c}
-${c}│ ❏ ${prefix}vjahat${c}
-${c}│ ❏ ${prefix}vnolep${c}
-${c}│ ❏ ${prefix}vwibu${c}
-${c}│ ❏ ${prefix}vhebat${c}
-${c}│ ❏ ${prefix}vpakboy${c}
-${c}│ ❏ ${prefix}vpakgirl${c}
-${c}╰─────────────────${c}
+	return `◉ *Kerang Menu*
+${c}  ❏ ${prefix}apakah${c}
+${c}  ❏ ${prefix}bisakah${c}
+${c}  ❏ ${prefix}kapankah${c}
+${c}  ❏ ${prefix}rate${c}
+${c}  ❏ ${prefix}ganteng${c}
+${c}  ❏ ${prefix}beban${c}
+${c}  ❏ ${prefix}cantik${c}
+${c}  ❏ ${prefix}jelek${c}
+${c}  ❏ ${prefix}nggagur${c}
+${c}  ❏ ${prefix}baik${c}
+${c}  ❏ ${prefix}jahat${c}
+${c}  ❏ ${prefix}nolep${c}
+${c}  ❏ ${prefix}wibu${c}
+${c}  ❏ ${prefix}hebat${c}
+${c}  ❏ ${prefix}pakboy${c}
+${c}  ❏ ${prefix}pakgirl${c}
+${c}  ❏ ${prefix}vganteng${c}
+${c}  ❏ ${prefix}vbeban${c}
+${c}  ❏ ${prefix}vcantik${c}
+${c}  ❏ ${prefix}vjelek${c}
+${c}  ❏ ${prefix}vnggagur${c}
+${c}  ❏ ${prefix}vbaik${c}
+${c}  ❏ ${prefix}vjahat${c}
+${c}  ❏ ${prefix}vnolep${c}
+${c}  ❏ ${prefix}vwibu${c}
+${c}  ❏ ${prefix}vhebat${c}
+${c}  ❏ ${prefix}vpakboy${c}
+${c}  ❏ ${prefix}vpakgirl${c}
           
        ~ PatrickBot 2021`
 }
 
 exports.fun = (prefix) => {
-	return `▪️ *Fun Menu*
-${c}│ ❏ ${prefix}tts${c}
-${c}│ ❏ ${prefix}bpfont${c}
-${c}│ ❏ ${prefix}truth${c}
-${c}│ ❏ ${prefix}dare${c}
-${c}│ ❏ ${prefix}leaderboard${c}
-${c}│ ❏ ${prefix}hobby${c}
-${c}│ ❏ ${prefix}memeindo${c}
-${c}│ ❏ ${prefix}asupan${c}
-${c}│ ❏ ${prefix}jadian${c}
-${c}│ ❏ ${prefix}artinama${c}
-${c}╰─────────────────${c}
+	return `◉ *Fun Menu*
+${c}  ❏ ${prefix}tts${c}
+${c}  ❏ ${prefix}emoji${c}
+${c}  ❏ ${prefix}bpfont${c}
+${c}  ❏ ${prefix}truth${c}
+${c}  ❏ ${prefix}dare${c}
+${c}  ❏ ${prefix}leaderboard${c}
+${c}  ❏ ${prefix}hobby${c}
+${c}  ❏ ${prefix}memeindo${c}
+${c}  ❏ ${prefix}asupan${c}
+${c}  ❏ ${prefix}jadian${c}
+${c}  ❏ ${prefix}artinama${c}
 
        ~ PatrickBot 2021`
 }
 
 exports.premium = (prefix) => {
-	return `▪️ *Premium Menu*
-${c}│ ❏ ${prefix}swm${c}
-${c}│ ❏ ${prefix}groupsearch${c}
-${c}│ ❏ ${prefix}take${c}
-${c}│ ❏ ${prefix}hidetag20${c}
-${c}╰─────────────────${c}
+	return `◉ *Premium Menu*
+${c}  ❏ ${prefix}swm${c}
+${c}  ❏ ${prefix}groupsearch${c}
+${c}  ❏ ${prefix}take${c}
+${c}  ❏ ${prefix}hidetag20${c}
 
        ~ PatrickBot 2021`
 }
 
 exports.berita = (prefix) => {
-	return `▪️ *Berita Menu*
-${c}│ ❏ ${prefix}sindo (internasional)${c}
-${c}│ ❏ ${prefix}sindo2 (nasional)${c}
-${c}│ ❏ ${prefix}okezone${c}
-${c}│ ❏ ${prefix}kompastv${c}
-${c}│ ❏ ${prefix}bbcnews${c}
-${c}│ ❏ ${prefix}beritanews${c}
-${c}╰─────────────────${c}
+	return `◉ *Berita Menu*
+${c}  ❏ ${prefix}sindo (internasional)${c}
+${c}  ❏ ${prefix}sindo2 (nasional)${c}
+${c}  ❏ ${prefix}okezone${c}
+${c}  ❏ ${prefix}kompastv${c}
+${c}  ❏ ${prefix}bbcnews${c}
+${c}  ❏ ${prefix}beritanews${c}
 
        ~ PatrickBot 2021`
 }
 
 exports.download = (prefix) => {
-	return `▪️ *Download Menu*
-${c}│ ❏ ${prefix}play${c}
-${c}│ ❏ ${prefix}ytmp3${c}
-${c}│ ❏ ${prefix}ytmp32${c}
-${c}│ ❏ ${prefix}ytmp4${c}
-${c}│ ❏ ${prefix}ytmp42${c}
-${c}│ ❏ ${prefix}tiktokdownload${c}
-${c}│ ❏ ${prefix}igvideo${c}
-${c}│ ❏ ${prefix}igphoto${c}
-${c}╰─────────────────${c}
+	return `◉ *Download Menu*
+${c}  ❏ ${prefix}ytplay${c}
+${c}  ❏ ${prefix}play${c}
+${c}  ❏ ${prefix}ytmp3${c}
+${c}  ❏ ${prefix}ytmp3v2${c}
+${c}  ❏ ${prefix}ytmp4${c}
+${c}  ❏ ${prefix}ytmp4v2${c}
+${c}  ❏ ${prefix}tiktokdownload${c}
+${c}  ❏ ${prefix}igvideo${c}
+${c}  ❏ ${prefix}igphoto${c}
 
        ~ PatrickBot 2021`
 }
 
 exports.search = (prefix) => {
-	return `▪️ *Search Menu*
-${c}│ ❏ ${prefix}gsmarena${c}
-${c}│ ❏ ${prefix}pinterest${c}
-${c}│ ❏ ${prefix}googleimage${c}
-${c}│ ❏ ${prefix}stickersearch${c}
-${c}│ ❏ ${prefix}aesthetic${c}
-${c}│ ❏ ${prefix}shopee${c}
-${c}│ ❏ ${prefix}jalantikus${c}
-${c}│ ❏ ${prefix}thelazy${c}
-${c}│ ❏ ${prefix}wattpad${c}
-${c}│ ❏ ${prefix}rexdl${c}
-${c}│ ❏ ${prefix}moddroid${c}
-${c}│ ❏ ${prefix}sfile${c}
-${c}╰─────────────────${c}
+	return `◉ *Search Menu*
+${c}  ❏ ${prefix}gsmarena${c}
+${c}  ❏ ${prefix}pinterest${c}
+${c}  ❏ ${prefix}googleimage${c}
+${c}  ❏ ${prefix}stickersearch${c}
+${c}  ❏ ${prefix}aesthetic${c}
+${c}  ❏ ${prefix}shopee${c}
+${c}  ❏ ${prefix}jalantikus${c}
+${c}  ❏ ${prefix}thelazy${c}
+${c}  ❏ ${prefix}wattpad${c}
+${c}  ❏ ${prefix}rexdl${c}
+${c}  ❏ ${prefix}moddroid${c}
+${c}  ❏ ${prefix}sfile${c}
 
        ~ PatrickBot 2021`
 }
 
 exports.group = (prefix) => {
-	return `▪️ *Group Menu*
-${c}│ ❏ ${prefix}enable${c}
-${c}│ ❏ ${prefix}disable${c} 
-${c}│ ❏ ${prefix}group buka | tutup${c}
-${c}│ ❏ ${prefix}add 62881xxxx${c}
-${c}│ ❏ ${prefix}promote @tag${c}
-${c}│ ❏ ${prefix}demote @tag${c}
-${c}│ ❏ ${prefix}setname teks${c}
-${c}│ ❏ ${prefix}setpp reply image${c}
-${c}│ ❏ ${prefix}setdesc teks${c}
-${c}│ ❏ ${prefix}groupinfo${c}
-${c}│ ❏ ${prefix}linkgrup${c}
-${c}│ ❏ ${prefix}tagall${c}
-${c}│ ❏ ${prefix}hidetag${c}
-${c}│ ❏ ${prefix}hidetag20${c}
-${c}│ ❏ ${prefix}hidetag50${c}
-${c}│ ❏ ${prefix}tagme${c}
-${c}│ ❏ ${prefix}level${c}
-${c}│ ❏ ${prefix}listonline${c}
-${c}│ ❏ ${prefix}delete${c}
-${c}│ ❏ ${prefix}caripesan${c}
-${c}╰─────────────────${c}
+	return `◉ *Group Menu*
+${c}  ❏ ${prefix}enable${c}
+${c}  ❏ ${prefix}disable${c} 
+${c}  ❏ ${prefix}group buka | tutup${c}
+${c}  ❏ ${prefix}add 62881xxxx${c}
+${c}  ❏ ${prefix}promote @tag${c}
+${c}  ❏ ${prefix}demote @tag${c}
+${c}  ❏ ${prefix}setname teks${c}
+${c}  ❏ ${prefix}setpp reply image${c}
+${c}  ❏ ${prefix}setdesc teks${c}
+${c}  ❏ ${prefix}groupinfo${c}
+${c}  ❏ ${prefix}linkgrup${c}
+${c}  ❏ ${prefix}tagall${c}
+${c}  ❏ ${prefix}hidetag${c}
+${c}  ❏ ${prefix}hidetag20${c}
+${c}  ❏ ${prefix}hidetag50${c}
+${c}  ❏ ${prefix}tagme${c}
+${c}  ❏ ${prefix}level${c}
+${c}  ❏ ${prefix}listonline${c}
+${c}  ❏ ${prefix}delete${c}
+${c}  ❏ ${prefix}caripesan${c}
           
        ~ PatrickBot 2021`
 }
 
 exports.islam = (prefix) => {
-	return `▪️ *Islam Menu*
-${c}│ ❏ ${prefix}quran${c}
-${c}│ ❏ ${prefix}quotesislami${c}
-${c}│ ❏ ${prefix}listsurah${c}
-${c}╰─────────────────${c}
+	return `◉ *Islam Menu*
+${c}  ❏ ${prefix}quran${c}
+${c}  ❏ ${prefix}quotesislami${c}
+${c}  ❏ ${prefix}listsurah${c}
           
        ~ PatrickBot 2021`
 }
 
 exports.cloud = (prefix) => {
-	return `*▪️ Cloud Menu*
-${c}│ ❏ ${prefix}addimage${c} 
-${c}│ ❏ ${prefix}addvn${c} 
-${c}│ ❏ ${prefix}addvideo${c} 
-${c}│ ❏ ${prefix}addstiker${c}  
-${c}│ ❏ ${prefix}getstiker${c}
-${c}│ ❏ ${prefix}getvn${c} 
-${c}│ ❏ ${prefix}getimage${c} 
-${c}│ ❏ ${prefix}getvideo${c}
-${c}│ ❏ ${prefix}listvideo${c} 
-${c}│ ❏ ${prefix}listvn${c} 
-${c}│ ❏ ${prefix}liststiker${c}
-${c}│ ❏ ${prefix}listimage${c}
-${c}╰────────────${c}
+	return `*◉ Cloud Menu*
+${c}  ❏ ${prefix}addimage${c} 
+${c}  ❏ ${prefix}addvn${c} 
+${c}  ❏ ${prefix}addvideo${c} 
+${c}  ❏ ${prefix}addstiker${c}  
+${c}  ❏ ${prefix}getstiker${c}
+${c}  ❏ ${prefix}getvn${c} 
+${c}  ❏ ${prefix}getimage${c} 
+${c}  ❏ ${prefix}getvideo${c}
+${c}  ❏ ${prefix}listvideo${c} 
+${c}  ❏ ${prefix}listvn${c} 
+${c}  ❏ ${prefix}liststiker${c}
+${c}  ❏ ${prefix}listimage${c}
           
        ~ PatrickBot 2021`
 }
 
 exports.other = (prefix) => {
-	return `▪️ *Other Menu*
-${c}│ ❏ ${prefix}sticker${c} 
-${c}│ ❏ ${prefix}tagstiker${c} 
-${c}│ ❏ ${prefix}nulis${c} 
-${c}│ ❏ ${prefix}swm${c} 
-${c}│ ❏ ${prefix}toimg${c}  
-${c}│ ❏ ${prefix}ssweb${c}
-${c}│ ❏ ${prefix}tomp3${c} 
-${c}│ ❏ ${prefix}bass${c}  
-${c}│ ❏ ${prefix}tupai${c}
-${c}│ ❏ ${prefix}slowmo${c} 
-${c}│ ❏ ${prefix}gemok${c}
-${c}╰────────────${c}
+	return `◉ *Other Menu*
+${c}  ❏ ${prefix}sticker${c} 
+${c}  ❏ ${prefix}tagstiker${c} 
+${c}  ❏ ${prefix}nulis${c} 
+${c}  ❏ ${prefix}swm${c} 
+${c}  ❏ ${prefix}toimg${c}  
+${c}  ❏ ${prefix}ssweb${c}
+${c}  ❏ ${prefix}tomp3${c} 
+${c}  ❏ ${prefix}shortlink${c} 
+${c}  ❏ ${prefix}shortlink2${c} 
+${c}  ❏ ${prefix}shortlink3${c}  
+${c}  ❏ ${prefix}bass${c}  
+${c}  ❏ ${prefix}tupai${c}
+${c}  ❏ ${prefix}slowmo${c} 
+${c}  ❏ ${prefix}gemok${c}
 
        ~ PatrickBot 2021`
 }
 
 exports.anime = (prefix) => {
-	return `▪️ *Anime Menu*
-${c}│ ❏ ${prefix}kdu${c} 
-${c}│ ❏ ${prefix}femdom${c}
-${c}│ ❏ ${prefix}waifukawai${c}  
-${c}│ ❏ ${prefix}kemonimimi${c}
-${c}│ ❏ ${prefix}kuni${c} 
-${c}│ ❏ ${prefix}husbu2${c}  
-${c}│ ❏ ${prefix}neko2${c}
-${c}│ ❏ ${prefix}holo${c} 
-${c}╰────────────${c}
+	return `◉ *Anime Menu*
+${c}  ❏ ${prefix}kdu${c} 
+${c}  ❏ ${prefix}femdom${c}
+${c}  ❏ ${prefix}waifukawai${c}  
+${c}  ❏ ${prefix}kemonimimi${c}
+${c}  ❏ ${prefix}kuni${c} 
+${c}  ❏ ${prefix}husbu2${c}  
+${c}  ❏ ${prefix}neko2${c}
+${c}  ❏ ${prefix}holo${c} 
 
-*▪️ NSFW Menu*
-${c}│ ❏ ${prefix}ero${c} 
-${c}│ ❏ ${prefix}nsfwpussy${c} 
-${c}│ ❏ ${prefix}nsfwyuri${c} 
-${c}│ ❏ ${prefix}solo${c}  
-${c}│ ❏ ${prefix}gecg${c}
-${c}│ ❏ ${prefix}avatar${c} 
-${c}│ ❏ ${prefix}dxd${c} 
-${c}╰────────────${c}
+*◉ NSFW Menu*
+${c}  ❏ ${prefix}ero${c} 
+${c}  ❏ ${prefix}nsfwpussy${c} 
+${c}  ❏ ${prefix}nsfwyuri${c} 
+${c}  ❏ ${prefix}solo${c}  
+${c}  ❏ ${prefix}gecg${c}
+${c}  ❏ ${prefix}avatar${c} 
+${c}  ❏ ${prefix}dxd${c} 
           
        ~ PatrickBot 2021`
 }
